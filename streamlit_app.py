da8f8b76faeda21d3bb2fcc3438448bf7e7b4db4
Phanwira <phanwira.p@ku.th>
2026-01-23 23:21:38 +0700

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from utils.capacity import calculate_capacity
 
-st.title("Hello, Streamlit!")
+st.title("Latex Management!")
 st.write("Welcome to your first Streamlit app.")
 st.button("Click me!")
 st.write("Capacity Calculation Example:")
