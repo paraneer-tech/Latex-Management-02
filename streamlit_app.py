1fa6f5421182019111ba77e8da44f9262b70cd23
Phanwira <phanwira.p@ku.th>
2026-01-23 16:57:21 +0700

    new file:   requirments.txt

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+from utils.capacity import calculate_capacity
+
+st.title("Hello, Streamlit!")
+st.write("Welcome to your first Streamlit app.")
+st.button("Click me!")
---
+++ utils/capacity.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+
+
+def calculate_capacity(input):
+    result = input * 2  # Example calculation
+    return result
